bb3c4dc080aa01486064ffee43ef5f927fbf7d3c
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-07-08 22:11:22 +0300

    убраны лишние импорты и методы

--- add_contact_test.py
+++ add_contact_test.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
 class AddContact(unittest.TestCase):
@@ -95,28 +93,7 @@ class AddContact(unittest.TestCase):
         wd.find_element_by_name("notes").send_keys("notes")
         wd.find_element_by_xpath("//input[@name='submit']").click()
         wd.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+
     def tearDown(self):
         self.wd.quit()
         self.assertEqual([], self.verificationErrors)
