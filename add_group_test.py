7f768923e498000254e36531454792c3d35556e4
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-07-13 20:36:12 +0300

    add_group_test переведен с unittest на py.test

--- add_group_test.py
+++ add_group_test.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from group import Group
 from application import Application
 
-class AddGroup(unittest.TestCase):
 
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="Test1", header="Test1", footer="Test1"))
-        self.app.logout()
 
-    def test_add_emty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="Test1", header="Test1", footer="Test1"))
+    app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
