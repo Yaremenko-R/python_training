53ae2beaa3a5a0e21dc7645a600dbb39b61596d0
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-07-13 19:19:58 +0300

    Занятие 2: Очередной рефакторинг

--- add_group_test.py
+++ add_group_test.py
@@ -16,6 +16,7 @@ class AddGroup(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -28,6 +29,7 @@ class AddGroup(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_group_page(wd)
         # Init group creation
         wd.find_element_by_name("new").click()
         # Fill group form
@@ -42,6 +44,7 @@ class AddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # Submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
@@ -51,20 +54,14 @@ class AddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="Test1", header="Test1", footer="Test1"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_emty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def is_element_present(self, how, what):
