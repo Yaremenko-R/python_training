8e8110f4f532431ea4cea5762e978e9c138aaa7f
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-07-20 22:52:18 +0300

    добавлена проверка существования группы перед редактированием

--- test/modify_group_test.py
+++ test/modify_group_test.py
@@ -3,12 +3,18 @@ from model.group import Group
 
 
 def test_modify_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name = "TOST"))
     app.group.modify_first_group(Group(name="Test222"))
 
 
 def test_modify_first_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name = "TOST"))
     app.group.modify_first_group(Group(header="Test222"))
 
 
 def test_modify_first_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name = "TOST"))
     app.group.modify_first_group(Group(footer="Test222"))
