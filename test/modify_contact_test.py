658c2411e1d86c63ede0aeb4aef97665ff5c0843
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-07-20 23:07:44 +0300

    добавлена проверка существования контакта перед редактированием

--- test/modify_contact_test.py
+++ test/modify_contact_test.py
@@ -3,4 +3,6 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
-    app.contact.modify_first_contact(Contact(firstname="fn500", bday="10", bmonth="February", byear="1000", aday="20", amonth="March", ayear="2000"))
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="fn500", bday="10", bmonth="February", byear="1000", aday="20", amonth="March", ayear="2000"))
+    app.contact.modify_first_contact(Contact(firstname="fn501", bday="11", bmonth="February", byear="1001", aday="21", amonth="March", ayear="2001"))
