da9411f75904b0a6cb54a057e8a3724e600bc8ce
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-07-21 23:07:21 +0300

    оптимизирован возврат на страницу групп

--- fixture/group.py
+++ fixture/group.py
@@ -8,6 +8,11 @@ class GroupHelper:
         if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
             wd.find_element_by_link_text("groups").click()
 
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("group page").click()
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
@@ -55,10 +60,6 @@ class GroupHelper:
         # Submit group creation
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
 
     def count(self):
         wd = self.app.wd
