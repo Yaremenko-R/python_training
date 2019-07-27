eac2b4cfa7a3154057df85c2d08b4b40b822176c
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-07-27 19:05:57 +0300

    добавлена сортировка перед сравнением при создании контакта

--- model/contact.py
+++ model/contact.py
@@ -1,8 +1,11 @@
+from sys import maxsize
+
+
 class Contact:
 
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None, address=None, homephone=None,
                  mobilephone=None, workphone=None, fax=None, email1=None, email2=None, email3=None, homepage=None, bday="10", bmonth="February", byear=None, aday="20", amonth="March",
-                 ayear=None, address2=None, homephone2=None, notes=None):
+                 ayear=None, address2=None, homephone2=None, notes=None, id=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -27,4 +30,16 @@ class Contact:
         self.address2 = address2
         self.homephone2 = homephone2
         self.notes = notes
+        self.id = id
 
+    def __repr__(self):
+        return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
+
+    def __eq__(self, other):
+        return  (self.id is None or other.id is None or self.id == other.id) and self.lastname == other.lastname and self.firstname == other.firstname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/add_contact_test.py
+++ test/add_contact_test.py
@@ -3,13 +3,25 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.contact.create(Contact(firstname="fnn", middlename="mn", lastname="ln", nickname="nn", title="title", company="company", address="address", homephone="home", mobilephone="mobile", workphone="work", fax="fax",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="fnn", middlename="mn", lastname="ln", nickname="nn", title="title", company="company", address="address", homephone="home", mobilephone="mobile", workphone="work", fax="fax",
                                email1="e1", email2="e2", email3="e3", homepage="homepage", bday="12", bmonth="February", byear="1990", aday="15", amonth="March", ayear="1995", address2="address",
-                               homephone2="home", notes="notes"))
+                               homephone2="home", notes="notes")
+    app.contact.create(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_add_empty_contact(app):
-    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", homephone="", mobilephone="", workphone="", fax="",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", homephone="", mobilephone="", workphone="", fax="",
                                email1="", email2="", email3="", homepage="", bday="12", bmonth="February", byear="", aday="15", amonth="March", ayear="", address2="",
-                               homephone2="", notes=""))
+                               homephone2="", notes="")
+    app.contact.create(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
