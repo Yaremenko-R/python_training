e0c21b34729d37040493cb08cb7c399a278540ff
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-07-27 19:17:28 +0300

    добавлена сортировка перед сравнением при удалении контакта

--- test/del_contact_test.py
+++ test/del_contact_test.py
@@ -3,6 +3,12 @@ from model.contact import Contact
 
 
 def test_delete_first_contact(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="fn500")
     if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="fn500"))
+        app.contact.create(contact)
     app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
