3c224f18d83992b9fa43cd3e87bdf5f6dc6ba374
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-07-27 19:12:52 +0300

    добавлена сортировка перед сравнением при изменении контакта

--- test/modify_contact_test.py
+++ test/modify_contact_test.py
@@ -3,6 +3,13 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="fn500")
+    contact.id = old_contacts[0].id
     if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="fn500"))
-    app.contact.modify_first_contact(Contact(firstname="fn501"))
+        app.contact.create(Contact(contact))
+    app.contact.modify_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
