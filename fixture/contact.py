6af17dd52e1f66759d175823ab3783083cec7287
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-07-27 21:46:42 +0300

    randrange на удаление контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -81,10 +81,17 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
+    def select_contact_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def delete_first_contact(self):
+        self.delete_contact_by_index(0)
+
+    def delete_contact_by_index(self, index):
         wd = self.app.wd
         self.back_to_contacts_list()
-        self.select_first_contact()
+        self.select_contact_by_index(index)
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
         self.back_to_contacts_list()
--- test/del_contact_test.py
+++ test/del_contact_test.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
+from random import randrange
 from model.contact import Contact
 
 
-def test_delete_first_contact(app):
-    old_contacts = app.contact.get_contact_list()
+def test_delete_some_contact(app):
     contact = Contact(firstname="fn500")
     if app.contact.count() == 0:
         app.contact.create(contact)
-    app.contact.delete_first_contact()
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    app.contact.delete_contact_by_index(index)
     assert len(old_contacts) - 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[0:1] = []
+    old_contacts[index:index+1] = []
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
