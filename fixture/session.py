e9d3523a7766f7faab024fc5dc409ff3c1c3d5d0
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-02 21:31:41 +0300

    оптимизация логина get_logged_user

--- fixture/session.py
+++ fixture/session.py
@@ -18,7 +18,7 @@ class SessionHelper:
         wd = self.app.wd
         self.app.open_home_page()
         self.fill_login_form(password, username)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+        wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def logout(self):
         wd = self.app.wd
@@ -30,7 +30,11 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]
 
     def ensure_logout(self):
         wd = self.app.wd
