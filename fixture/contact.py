fd40835a13fa5a6cceb7a7c755e836cc1c659f86
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-03 16:50:22 +0300

    добавлен тест сравнения телефонов на view_page

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.support.ui import Select
 from model.contact import Contact
-
+import re
 
 class ContactHelper:
 
@@ -161,6 +161,21 @@ class ContactHelper:
         homephone2 = wd.find_element_by_name("phone2").get_attribute("value")
         return Contact(firstname=fname, lastname=lname, id=id, homephone=homephone, workphone=workphone,
                        mobilephone=mobilephone, homephone2=homephone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        homephone2 = re.search("P: (.*)", text).group(1)
+        return Contact(homephone=homephone, workphone=workphone, mobilephone=mobilephone, homephone2=homephone2)
+
+
+
+
+
 
 
 
