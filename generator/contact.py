fb46154fdc2c27aab8e9cca013ce6e6208a666fb
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-10 21:58:38 +0300

    добавлена параметризация(-n, -f) для контактов

--- generator/contact.py
+++ generator/contact.py
@@ -1,8 +1,26 @@
 from model.contact import Contact
 import random
 import string
-import  os.path
+import os.path
 import json
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 3
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
 
 # Fixed data
 # from data.contact import constant as test_data добавить в файл add_contact_test.py
@@ -31,9 +49,9 @@ test_data = [Contact(firstname="", middlename="", lastname="", nickname="", titl
                   email3=random_string("e3@", 10), homepage=random_string("www.homepage", 15), bday="12", bmonth="February",
                   byear=random_string("1999", 5), aday="15", amonth="March", ayear=random_string("2000", 5), address2=random_string("address2", 15),
                   homephone2=random_string("345", 10), notes=random_string("notes", 25))
-    for i in range(3)]
+    for i in range(n)]
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/contacts.json")
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as f:
-    f.write(json.dumps(test_data, default=lambda x: x.__dict__, indent=2))
+with open(file, "w") as out:
+    out.write(json.dumps(test_data, default=lambda x: x.__dict__, indent=2))
