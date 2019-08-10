10f66900ff011dfccba1338ca9992d74cf2165af
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-10 23:01:00 +0300

    добавлена динамическая генерация для контактов

--- test/add_contact_test.py
+++ test/add_contact_test.py
@@ -1,10 +1,12 @@
 from model.contact import Contact
-import pytest
-from data.contacts import test_data
+# import pytest
+# from data.contacts import test_data
 
 
-@pytest.mark.parametrize("contact", test_data, ids=[repr(x) for x in test_data])
-def test_add_contact(app, contact):
+# @pytest.mark.parametrize("contact", test_data, ids=[repr(x) for x in test_data])
+
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
