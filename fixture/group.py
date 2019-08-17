02f39bdd4ae74e979e51483072b6641d4d4afe23
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-17 21:39:13 +0300

    загрузка данных из бд для проверок, тесты: add_group, del_group

--- fixture/group.py
+++ fixture/group.py
@@ -24,6 +24,10 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
 
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
     def create(self, group):
         wd = self.app.wd
         self.open_group_page()
@@ -53,6 +57,14 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         self.select_group_by_index(index)
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_group_by_id(id)
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
--- test/del_group_test.py
+++ test/del_group_test.py
@@ -1,15 +1,14 @@
-from random import randrange
+import random
 from model.group import Group
 
 
-def test_delete_some_group(app):
+def test_delete_some_group(app, db):
     group = Group(name="TEST")
-    if app.group.count() == 0:
+    if db.get_group_list() == 0:
         app.group.create(group)
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    app.group.delete_group_by_index(index)
-    assert len(old_groups) - 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index:index+1] = []
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    app.group.delete_group_by_id(group.id)
+    new_groups = db.get_group_list()
+    old_groups.remove(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
