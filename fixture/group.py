83ef352876499144cd6517182ccd2607634fdc70
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-18 00:02:08 +0300

    загрузка данных из бд для проверок, тесты: modify_group

--- fixture/group.py
+++ fixture/group.py
@@ -82,6 +82,16 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def modify_group_by_id(self, id, new_group_data):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_group_by_id(id)
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(Group(name="XXX", header="YYY", footer="ZZZ"))
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def count(self):
         wd = self.app.wd
         self.open_group_page()
--- test/modify_group_test.py
+++ test/modify_group_test.py
@@ -1,16 +1,14 @@
-from random import randrange
+import random
 from model.group import Group
 
 
-def test_modify_some_group(app):
+def test_modify_some_group(app, db):
     group = Group(name = "TOST")
-    if app.group.count() == 0:
+    if db.get_group_list() == 0:
         app.group.create(group)
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
+    old_groups = db.get_group_list()
+    rgroup = random.choice(old_groups)
+    app.group.modify_group_by_id(rgroup.id, group)
+    new_groups = db.get_group_list()
+    old_groups[rgroup.id] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
