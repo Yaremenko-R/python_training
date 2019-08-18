95bc908207d7688204f6cb2f4b4dcbb86e485be0
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-18 20:39:56 +0300

    delete_contact переведен на данные из бд + check_ui

--- test/del_contact_test.py
+++ test/del_contact_test.py
@@ -3,14 +3,15 @@ from random import randrange
 from model.contact import Contact
 
 
-def test_delete_some_contact(app):
+def test_delete_some_contact(app, db, check_ui):
     contact = Contact(firstname="fn500", lastname="ln")
-    if app.contact.count() == 0:
+    if db.get_contact_list() == 0:
         app.contact.create(contact)
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     index = randrange(len(old_contacts))
     app.contact.delete_contact_by_index(index)
-    assert len(old_contacts) - 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     old_contacts[index:index+1] = []
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
