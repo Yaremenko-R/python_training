51902f0200d4f1c3c5492ffd271039d9e0ff5ab4
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-18 19:58:49 +0300

    получение контактов из бд + тест:контакты бд vs ui

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from model.group import Group
+from model.contact import Contact
 
 class DbFixture:
 
@@ -18,6 +19,23 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, middlename, lastname, nickname, company, title, address, home, mobile, work, "
+                           "fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, " 
+                           "address2, phone2, notes from addressbook where deprecated = '0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, middlename, lastname, nickname, company, title, address, home, mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear,
+                 address2, phone2, notes) = row
+                list.append(Contact(id=str(id), firstname=firstname, middlename=middlename, lastname=lastname, nickname=nickname, company=company, title=title, address=address, homephone=home,
+                                    mobilephone=mobile, workphone=work, fax=fax, email1=email, email2=email2, email3=email3, homepage=homepage, bday=bday, bmonth=bmonth, byear=byear,
+                                    aday=aday, amonth=amonth, ayear=ayear, address2=address2, homephone2=phone2, notes=notes))
         finally:
             cursor.close()
         return list
--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,5 +1,5 @@
 from model.group import Group
-
+from  model.contact import Contact
 
 def test_group_list(app, db):
     ui_list = app.group.get_group_list()
@@ -9,3 +9,9 @@ def test_group_list(app, db):
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
 
 
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+    def clean(contact):
+        return Contact(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip())
+    db_list = map(clean, db.get_contact_list())
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
