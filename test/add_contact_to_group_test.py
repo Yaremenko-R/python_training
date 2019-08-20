b991e56d410231700c7fbb18c2fae0533b258d7b
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-20 20:06:23 +0300

    test_add_some_contact_to_some_group(without checks) v.3 перевод на ormfixture

--- test/add_contact_to_group_test.py
+++ test/add_contact_to_group_test.py
@@ -6,14 +6,14 @@ from random import randrange
 def test_add_some_contact_to_some_group(app, db, orm):
     contact = Contact(firstname="fn500", lastname="ln")
     group = Group(name="TEST")
-    if db.get_contact_list() == 0:
+    if orm.get_contact_list() == 0:
         app.contact.create(contact)
-    elif db.get_group_list() == 0:
+    elif orm.get_group_list() == 0:
         app.group.create(group)
     else:
-        contacts = db.get_contact_list()
+        contacts = orm.get_contact_list()
         cindex = randrange(len(contacts))
-        groups = db.get_group_list()
+        groups = orm.get_group_list()
         gindex = randrange(len(groups))
         group = groups[gindex]
         app.contact.add_contact_by_index_to_group(cindex, gindex)
