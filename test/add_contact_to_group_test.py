9cb1af7c105964a7a7b507136e75cfdd7f1e1a9c
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-27 00:15:08 +0300

    test_add_some_contact_to_some_group fix

--- test/add_contact_to_group_test.py
+++ test/add_contact_to_group_test.py
@@ -4,7 +4,11 @@ from random import randrange
 
 
 def test_add_some_contact_to_some_group(app, orm):
-    contact = Contact(firstname="fn500", lastname="ln")
+    contact = Contact(firstname="Python", lastname="Testing")
+    group = Group(name="Course")
+    if orm.get_group_list() == 0 or orm.get_contact_list() == 0:
+        app.group.create(group)
+        app.contact.create(contact)
     contacts = orm.get_contact_list()
     cindex = randrange(len(contacts))
     contact_to_add = contacts[cindex]
@@ -13,16 +17,10 @@ def test_add_some_contact_to_some_group(app, orm):
     group_to_add = groups[gindex]
     if orm.get_group_list() == sorted(orm.get_groups_contact_added(contact_to_add), key=Group.id_or_max):
         app.contact.create(contact)
-        app.contact.add_contact_by_index_to_group(0, gindex)
-        contacts_in_group_by_db = orm.get_contacts_in_group(group_to_add)
-        if contact in contacts_in_group_by_db:
-            print(f"Contact {contact} was added to group {group_to_add} successfully!")
-        else:
-            print("Something is wrong...")
-    else:
-        app.contact.add_contact_by_index_to_group(cindex, gindex)
-        contacts_in_group_by_db = orm.get_contacts_in_group(group_to_add)
-        if contact_to_add in contacts_in_group_by_db:
-            print(f"Contact {contact_to_add} was added to group {group_to_add} successfully!")
-        else:
-            print("Something is wrong...")
+        contact_to_add = contact
+        contacts.append(contact)
+        cindex = -1
+    app.contact.add_contact_by_index_to_group(cindex, gindex)
+    target_contact_groups = sorted(orm.get_groups_contact_added(contact_to_add), key=Group.id_or_max)
+    contacts_in_target_group = sorted(orm.get_contacts_in_group(group_to_add), key=Group.id_or_max)
+    assert contacts_in_target_group in target_contact_groups
