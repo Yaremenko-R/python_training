3e2301dd3a8c6a126cd811d33e79a1054d81b531
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-08-27 22:51:12 +0300

    test_del_some_contact_from_group fix2 - добавлена проверка предусловий

--- test/del_contact_from_group_test.py
+++ test/del_contact_from_group_test.py
@@ -1,7 +1,14 @@
+from model.contact import Contact
+from model.group import Group
 from random import randrange
 
 
 def test_del_some_contact_from_group(app, orm):
+    contact = Contact(firstname="Python", lastname="Testing")
+    group = Group(name="Course")
+    if orm.get_group_list() == 0 or orm.get_contact_list() == 0:
+        app.group.create(group)
+        app.contact.create(contact)
     contacts = orm.get_contact_list()
     cindex = randrange(len(contacts))
     contact_to_del = contacts[cindex]
